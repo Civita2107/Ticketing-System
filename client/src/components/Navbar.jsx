17e28427bfdcb365d9d21ec270e25089b50e38f8
Civita2107 <f.civitareale@gmail.com>
2024-06-28 19:47:36 +0200

    Moved TICKETING SYSTEM to left and added the link to /

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 import { LoginButton, LogoutButton } from './Authentication'
 import { FaSignInAlt, FaTicketAlt, FaUserCircle } from 'react-icons/fa';
 import { BsTicketDetailed } from 'react-icons/bs';
@@ -12,10 +13,10 @@ function NavBar(props) {
     <>
       <Navbar fixed="top" className='ticket-navbar'>
         <Container fluid>
-          <Navbar.Brand href="/" className="col-md-4 d-flex justify-content-start"><BsTicketDetailed style={{ paddingRight: '', color: '#ffffff' }} size={30} />
-          </Navbar.Brand>
-          <div className="d-flex justify-content-center">
-            <span style={{ color: '#ffffff', fontSize: '20px' }}>TICKETING SYSTEM </span>
+          <BsTicketDetailed style={{ paddingRight: '10px', color: '#ffffff' }} size={35} />
+          
+          <div className="d-flex justify-content-center text-center">
+            <Link to="/" style={{ color: '#ffffff', fontSize: '22px', textDecoration: 'none' }} >TICKETING SYSTEM </Link>
           </div>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse className="justify-content-end">
@@ -23,7 +24,7 @@ function NavBar(props) {
               {props.loggedIn ? (
                 <>
                   <FaUserCircle style={{ marginTop: '6px', marginRight: '1rem' }} />
-                  <span style={{ marginRight: '1rem' }}>{props.user.username}</span>
+                  <span style={{ marginRight: '1rem', fontSize: '22px' }}>{props.user.username}</span>
                   <LogoutButton logout={props.logout} />
                 </>
               ) : (
