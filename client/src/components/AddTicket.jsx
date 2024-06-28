598e42264d20e1083498329e47d9bb0d6900eb2c
Civita2107 <f.civitareale@gmail.com>
2024-06-28 19:35:19 +0200

    Fixed that when a token expires i couldn't add a ticket anymore. Now asks for a new token if the precedent token is no longer valid

--- client/src/components/AddTicket.jsx
+++ client/src/components/AddTicket.jsx
@@ -4,101 +4,111 @@ import { useState } from 'react';
 import API from '../API';
 
 const TicketForm = (props) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [errorMessage, setErrorMessage] = useState('');
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
-    const [category, setCategory] = useState('');
-    const [modalShow, setModalShow] = useState(false);
-    const [estimation, setEstimation] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [category, setCategory] = useState('');
+  const [modalShow, setModalShow] = useState(false);
+  const [estimation, setEstimation] = useState('');
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-        if (!title) {
-            setErrorMessage('Title is required');
-        } else if (!content) {
-           setErrorMessage('Content is required');
-        } else if (!category || category === 'Category') {
-            setErrorMessage('Category is required');
-        } else if (title.length > 40) {
-            setErrorMessage('Title is too long');
-        } else if (content.length > 400) {
-            setErrorMessage('Content is too long');
-        } else {
-            const toPass = { title: title, category: category };
-            console.log(props.authToken);
-            const newStat = await API.getStats(props.authToken, toPass);
+    if (!title) {
+      setErrorMessage('Title is required');
+    } else if (!content) {
+      setErrorMessage('Content is required');
+    } else if (!category || category === 'Category') {
+      setErrorMessage('Category is required');
+    } else if (title.length > 40) {
+      setErrorMessage('Title is too long');
+    } else if (content.length > 400) {
+      setErrorMessage('Content is too long');
+    } else {
+      const toPass = { title: title, category: category };
+      let newStat;
+      try {
+        let newStat = await API.getStats(props.authToken, toPass);
+        setEstimation(newStat.result);
+        setModalShow(true);
+      } catch (err) {
+        API.getAuthToken().then((resp) => {
+          props.setAuthToken(resp.token);
+          API.getStats(resp.token, toPass).then((newStat) => {
             setEstimation(newStat.result);
             setModalShow(true);
-        }
+          })
+        });
+      }
     }
+  }
 
-    const handleConfirm = () => {
-        const ticket = {
-            title: title,
-            content: content,
-            category: category
-        }
-        props.addTicket(ticket);
-        setModalShow(false);
-        navigate('/');
+  const handleConfirm = () => {
+    const ticket = {
+      title: title,
+      content: content,
+      category: category
     }
+    props.addTicket(ticket);
+    setModalShow(false);
+    navigate('/');
+  }
 
-    const handleCancel = () => {
-        setModalShow(false);
-    }
+  const handleCancel = () => {
+    setModalShow(false);
+  }
 
-    return (
-        <>
-        {errorMessage ? <Alert variant='danger' onClose={ () => setErrorMessage('')} dismissible>{errorMessage}</Alert> : false}
-        <Form onSubmit={handleSubmit} className='form'>
-      <Form.Group className="mb-3">
-        <Form.Label>Title</Form.Label>
-        <Form.Control placeholder="Title" onChange={(e) => setTitle(e.target.value)}/>
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Content</Form.Label>
-        <Form.Control as="textarea" placeholder="Content" onChange={(e) => setContent(e.target.value)}/>
+  return (
+    <>
+      {errorMessage ? <Alert variant='danger' onClose={() => setErrorMessage('')} dismissible>{errorMessage}</Alert> : false}
+      <Form onSubmit={handleSubmit} className='form'>
+        <Form.Group className="mb-3">
+          <Form.Label>Title</Form.Label>
+          <Form.Control placeholder="Title" onChange={(e) => setTitle(e.target.value)} />
         </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Category</Form.Label>
-        <Form.Select onChange={(e) => setCategory(e.target.value)}>
-          <option value="Category">Category</option>
-          <option value="inquiry">Inquiry</option>
-          <option value="maintainance">Maintainance</option>
-          <option value="new feature">New feature</option>
-          <option value="administrative">Administrative</option>
-          <option value="payment">Payment</option>
-        </Form.Select>
-      </Form.Group>
-      <Button variant="primary" type="submit" style={{marginRight: '10px'}}>Submit</Button>
-      <Button variant="secondary" onClick={() => navigate('/')}>Cancel</Button>
-    </Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Content</Form.Label>
+          <Form.Control as="textarea" placeholder="Content" onChange={(e) => setContent(e.target.value)} />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Category</Form.Label>
+          <Form.Select onChange={(e) => setCategory(e.target.value)}>
+            <option value="Category">Category</option>
+            <option value="inquiry">Inquiry</option>
+            <option value="maintainance">Maintainance</option>
+            <option value="new feature">New feature</option>
+            <option value="administrative">Administrative</option>
+            <option value="payment">Payment</option>
+          </Form.Select>
+        </Form.Group>
+        <Button variant="primary" type="submit" style={{ marginRight: '10px' }}>Submit</Button>
+        <Button variant="secondary" onClick={() => navigate('/')}>Cancel</Button>
+      </Form>
 
-    <div
-      className="modal show"
-      style={{ display: 'block', position: 'initial' }}
-    >
-      <Modal show={modalShow} onHide={handleCancel} keyboard={false} backdrop={false} >
-        <Modal.Header closeButton>
-          <Modal.Title>Add ticket confirmation</Modal.Title>
-        </Modal.Header>
+      <div
+        className="modal show"
+        style={{ display: 'block', position: 'initial' }}
+      >
+        <Modal show={modalShow} onHide={handleCancel} keyboard={false} backdrop={false} >
+          <Modal.Header closeButton>
+            <Modal.Title>Add ticket confirmation</Modal.Title>
+          </Modal.Header>
 
-        <Modal.Body>
-          <p>Title: {title}</p>
-          <p>Content: {content}</p>
-          <p>Category: {category}</p>
-          <p>Resolution estimation: {props.user.admin ? `${Math.floor(estimation / 24)} days ${estimation % 24} hours` : `${estimation} days`}</p>
-        </Modal.Body>
+          <Modal.Body>
+            <p>Title: {title}</p>
+            <p>Content: {content}</p>
+            <p>Category: {category}</p>
+            <p>Resolution estimation: {props.user.admin ? `${Math.floor(estimation / 24)} days ${estimation % 24} hours` : `${estimation} days`}</p>
+          </Modal.Body>
 
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCancel}>Close</Button>
-          <Button variant="primary" onClick={handleConfirm}>Confirm</Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCancel}>Close</Button>
+            <Button variant="primary" onClick={handleConfirm}>Confirm</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     </>
   );
 }
